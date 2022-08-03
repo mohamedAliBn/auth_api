b4e6622e78fb26514abc0a28c8da83fcb7898691
mohamedAliBn <mohamed3alibnjhhh@gmail.com>
2022-08-03 07:16:38 +0100

    add the security packages like cors helmet xss-clean ..

--- app.js
+++ app.js
@@ -8,8 +8,24 @@ const connectDB=require('./db/connect');
 const authRotuer=require("./routes/user")
 const authorization=require("./middlewares/authontication")
 
+// extra security package
+const cors=require("cors");
+const helmet=require("helmet");
+const xssClean=require("xss-clean");
+const rateLimit=require("express-rate-limit");
+
 //middlewares
+app.set("trust proxy",1)
 app.use(express.json())
+app.use(cors())
+app.use(helmet());
+app.use(xssClean())
+app.use(rateLimit({
+    windowMs:15 * 60 * 1000,
+    max:100
+}))
+
+// routes
 app.use("/api/v1/auth",authRotuer)
 app.get("/",(req,res)=> {
     res.send("<h1>auth api</h1>")
@@ -18,6 +34,7 @@ app.get("/api/v1/test",authorization,(req,res)=> {
     res.status(200).json({msg:"this is a test"})
 })
 
+// connect to db
 const port=process.env.PORT || 8080
 const start=async ()=> {
     try {
